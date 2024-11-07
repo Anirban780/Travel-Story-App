1d85ab9ab3f4dbf85f8311b04acd9a1fb4b8e194
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2024-11-07 22:48:26 +0530

    6th change

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import PasswordInput from '../../components/Input/PasswordInput';
 import { useNavigate } from 'react-router-dom';
 import { validateEmail } from '../../utils/helper';
 import axiosInstance from '../../utils/axiosInstance';
+import bgImage from '../../assets/Images/bg-image.png';
+
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -61,7 +63,10 @@ const Login = () => {
             <div className='login-ui-box bg-cyan-200 -bottom-40 right-1/2' />
 
             <div className='container h-screen flex items-center justify-center px-20 mx-auto'>
-                <div className='w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50'>
+                <div
+                    style={{ backgroundImage: `url(${bgImage})` }}
+                    className='w-2/4 h-[90vh] flex items-end bg-cover bg-center rounded-lg p-10 z-50'>
+                    
                     <div>
                         <h4 className='text-5xl text-white font-semibold leading-[58px]'>
                             Capture Your <br /> Journeys
