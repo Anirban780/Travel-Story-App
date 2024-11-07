21f237e79da893d197f0ebd60188624557335c31
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2024-11-07 23:57:34 +0530

    11th change

--- server/index.js
+++ server/index.js
@@ -25,6 +25,10 @@ const backendUrl = process.env.VITE_BACKEND_URL || 'http://localhost:8000';  // 
 // Serve static files from the assets folder
 app.use('/assets', express.static('assets'));
 
+app.get('/', (req, res) => {
+    res.status(200).json("Welcome to Backend Part of Travel Story App");
+});
+
 // CORS configuration for allowing both frontend URLs
 app.use(cors({
     origin: [
